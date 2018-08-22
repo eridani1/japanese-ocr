6d626fb27548a4b0da788c83065316deb7d508b4
Sean Yau <syau02@qub.ac.uk>
2018-08-22 14:27:35 +0100

    Added comments and removed redundant code

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ var helmet = require('helmet');
 
 app.use(helmet());
 
+// serve files in public folder
 app.use(express.static(__dirname + '/public'));
 
+// route url to mobile page
 app.get('/mobile', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/mobile.html'));
 });
@@ -16,30 +18,37 @@ app.get('/mobile', function(req, res) {
 io.on('connection', function(socket){
   console.log('Connection established');
   
+  // retrieving file from a URL
   socket.on("URL",function(address){
 		var httpfile = require('http');
 		var httpsfile = require('https');
 
+		// check if URL is http or https
 		var client = httpfile;
 		if (address.indexOf("https") === 0){
 			client = httpsfile;
 		}
 
-		client.get(address, (resp) => {
+		client.get(address, (resp) => 
+			// convert image to base64
 			resp.setEncoding('base64');
 			body = "data:" + resp.headers["content-type"] + ";base64,";
 			resp.on('data', (data) => { body += data});
 			resp.on('end', () => {				
+				// return base64 data to client
 				socket.emit("URL", body);
 			});
 		}).on('error', (e) => {
 			console.log(`Error: ${e.message}`);
 		});	 
   });
-  
+
+  // process OCR functions server-side for mobile use
   socket.on("scan",function(dataObject){	
 	var Tesseract = require('tesseract.js');
+	// format base64 data
 	var base64 = dataObject.imageData.replace(/^data:image\/jpeg;base64,/,"");
+	// convert base64 to buffer object
 	var imageBuffer = Buffer.from(base64,'base64');
 		
 	Tesseract.recognize(imageBuffer, {
